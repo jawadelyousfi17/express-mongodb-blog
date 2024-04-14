73ecbedfa8c5c56f4d2cbac9351ff57bcdaeff13
jawadelyousfi17 <35626062+jawadelyousfi17@users.noreply.github.com>
2024-04-14 09:01:22 +0100

    Update conectdb.js

--- db/conectdb.js
+++ db/conectdb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const dburl = process.env.DB_URI || 'mongodb+srv://jawadpro17:Jawad.18@ecomerce-api-db.ayaobei.mongodb.net/blog?retryWrites=true&w=majority&appName=ecomerce-api-db' 
+const dburl = process.env.DB_URI 
 
 const conectDb = async (conected) => mongoose.connect(dburl).then(
     (result) => {
